0aab3cdf2daeb07e1c942358e3058def893d2469
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2021-02-09 23:06:41 -0500

    Added Project file

---
+++ main.py
@@ -0,0 +1,67 @@
+import pyttsx3
+import PyPDF2
+
+reader = pyttsx3.init()  # object creation
+""" RATE"""
+rate = reader.getProperty('rate')  # getting details of current speaking rate
+print("Current voice rate" + str(rate))  # printing current voice rate
+reader.setProperty('rate', 125)  # setting up new voice rate
+
+"""VOLUME"""
+volume = reader.getProperty('volume')  # getting to know current volume level (min=0 and max=1)
+print("Current volume level : " + str(volume))  # printing current volume level
+reader.setProperty('volume', 1.0)  # setting up volume level  between 0 and 1
+print("Setting volume level to 100%.")  # printing current volume level
+
+"""VOICE"""
+voices = reader.getProperty('voices')  # getting details of current voice
+# reader.setProperty('voice', voices[1].id)  # changing index, changes voices. 1 for female
+reader.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male
+
+
+def read(text):
+    reader.say(text)
+    reader.runAndWait()
+    reader.stop()
+    return text
+
+
+file = open('resources/Designing Data Intensive Applications.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(file)
+print("Total Number of pages: " + str(pdfReader.numPages))
+pageNo = 115
+
+
+def get_page(number):
+    page = pdfReader.getPage(number)
+    # Extracting text from page
+    # And splitting it into chunks of lines
+    text = page.extractText()
+
+    # Iterate through each line and store into lines
+    lines = []
+    for line in text:
+        # Loop through each character of the line and add gap when , and . is present.
+        # Also ignore if there ae any special character.
+        for char in line:
+            if char == '.':
+                lines.append(".\n\n")
+            elif char == ',':
+                lines.append(",\n")
+            elif char != '-':
+                lines.append(char)
+    return ''.join(lines)
+
+
+def start_reading_file():
+    print(read("Starting the Reader"))
+    for cur_page_no in range(pageNo - 1, pdfReader.numPages):
+        # Creating a page object
+        cur_page_lines = get_page(cur_page_no)
+        print(read(cur_page_lines))
+
+
+# read("This is a test message.")
+start_reading_file()
+# get_page(111)
+# get_page(112)
